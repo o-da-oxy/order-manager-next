4a4509d2a45c8b72b1802b7b11d6f33346638ee4
Оксана Краснодемская <demskaya01@gmail.com>
2024-10-16 00:40:28 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.ctas}>
+          <h1>Cистема управления заказами и доставкой товаров</h1>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+      </footer>
+    </div>
+  );
+}
